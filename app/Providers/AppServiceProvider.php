b41e0f1714b653fe54cf633d4838d78892e08a4a
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-04-29 19:05:02 +0430

    Semantic network of words project start - @admin directive created, upload excel file to database in progress

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class   AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void {
+        // custom direction
+        Blade::directive('admin', static function () {
+            return "<?php if(auth()->check() && auth()->user()->user_type === 'admin'): ?>";
+        });
+
+        Blade::directive('endadmin', static function () {
+            return '<?php endif; ?>';
+        });
+    }
+}
