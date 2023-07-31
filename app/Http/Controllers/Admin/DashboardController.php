885d521a4dd9f02c9ec892baabaff44bc8569a0e
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-07-31 18:44:52 +0330

    upload word with txt file completed

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Exports\ExportWord;
 use App\Http\Controllers\Controller;
 use App\Imports\ImportWord;
 use App\Models\Word;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use JetBrains\PhpStorm\NoReturn;
+use Illuminate\Validation\Rules\File;
 use Maatwebsite\Excel\Facades\Excel;
 
 class DashboardController extends Controller
@@ -95,10 +94,32 @@ class DashboardController extends Controller
      */
     public function storeFile(Request $request): \Illuminate\Http\RedirectResponse
     {
+        $validated = $request->validate([
+            'file' => ['required', File::types(['xlsx', 'txt'])]
+        ]);
 
-        Excel::import(new ImportWord(),
-            $request->file('file')->store('files'));
+        $uploaded_file = ($request->file('file'));
 
+        switch ($uploaded_file->extension()) {
+            case 'txt':
+                $url = $uploaded_file;
+                $lines = file($url, FILE_IGNORE_NEW_LINES);
+                $arrays = array_map(function ($array) {
+                    $columns = ['word'];
+                    return array_combine($columns, array_map('trim', $array));
+                }, array_chunk($lines, 1));
+
+                $request->file('file')->store('files');
+                Word::query()->insert($arrays);
+
+                break;
+            case 'xlsx':
+                Excel::import(new ImportWord(), $request->file('file')->store('files'));
+                break;
+            default:
+                dd('hi');
+
+        }
         return to_route('admin.word.index');
     }
 }
